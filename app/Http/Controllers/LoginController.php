2126a2094da0b740dfbb98537c3c9eca18d463d2
andiabdillahdev <andiabdillahdev@gmail.com>
2024-12-13 18:38:05 +0800

    perbaikan current user

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -122,10 +122,14 @@ class LoginController extends BaseController
                     $data->waktu_masuk = '08:00:00';
                     $data->waktu_keluar = date('N') == 5 ? '15:30:00' : '15:00:00';
                     $data->waktu_apel = Carbon::parse($tanggal_hari_ini)->dayOfWeek === Carbon::MONDAY ? '07:15:00' : '07:40:00';
-                }else{
+                }elseif($data->tipe_pegawai == 'tenaga_kesehatan'){
                     $data->waktu_masuk = '08:00:00';
                     $data->waktu_keluar = '13:00:00';
                     $data->waktu_apel = '08:00:00';
+                }elseif ($data->tipe_pegawai == 'tenaga_pendidik' || $data->tipe_pegawai == 'tenaga_pendidik_non_guru') {
+                    $data->waktu_masuk = '08:00:00';
+                    $data->waktu_keluar = '14:00:00';
+                    $data->waktu_apel = '07:30:00';
                 }                
             }
             
