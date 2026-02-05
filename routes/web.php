5258e2a381f9542f9c67f5e5324fa33870cd9ab1
zhorahmatt <info.rahmathidayat@gmail.com>
2026-02-05 14:35:14 +0800

    feat: Relocated report routes from API to web and updated Composer dependencies.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LaporanApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('set-laporan')->group(function () {
+    Route::get('/laporan-kehadiran-pegawai', [LaporanApiController::class, 'export_pegawai_bulan'])->name('setlaporan.laporan.kehadiran.export');
+    Route::get('/laporan-kinerja-pegawai', [LaporanApiController::class, 'export_to_kinerja_pegawai'])->name('setlaporan.laporan.kinerja.export');
+    Route::get('/laporan-tpp-pegawai', [LaporanApiController::class, 'export_to_tpp_pegawai'])->name('setlaporan.laporan.tpp.export');
 });
